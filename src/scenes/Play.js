1279d4b2b7b35a6faa3c77b473b463a36b29fe74
CallMeCalvin101 <calvinkurniadjaja@gmail.com>
2022-08-08 01:01:39 -0700

    edited picking up and plaing down

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -12,6 +12,18 @@ class Play extends Phaser.Scene {
         keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
         keyE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.E);
 
+        this.exhibitGroup = this.add.group({
+            runChildUpdate: true
+        });
+
+        this.testExh1 = new Exhibit(this, 270, 100);
+        this.testExh2 = new Exhibit(this, 135, 100);
+        this.testExh3 = new Exhibit(this, 405, 100);
+
+        this.exhibitGroup.add(this.testExh1);
+        this.exhibitGroup.add(this.testExh2);
+        this.exhibitGroup.add(this.testExh3);
+
         this.player = new Player(this, 270, 480);
 
         this.artifactGroup = this.add.group({
@@ -26,18 +38,6 @@ class Play extends Phaser.Scene {
         this.artifactGroup.add(this.testArt2);
         this.artifactGroup.add(this.testArt3);
 
-        this.exhibitGroup = this.add.group({
-            runChildUpdate: true
-        });
-
-        this.testExh1 = new Exhibit(this, 270, 100);
-        this.testExh2 = new Exhibit(this, 135, 100);
-        this.testExh3 = new Exhibit(this, 405, 100);
-
-        this.exhibitGroup.add(this.testExh1);
-        this.exhibitGroup.add(this.testExh2);
-        this.exhibitGroup.add(this.testExh3);
-
         this.initArtifacts();
     }
 
@@ -49,26 +49,36 @@ class Play extends Phaser.Scene {
     initArtifacts() {
         for (let type of this.artifactGroup.getChildren()) {
             this.physics.add.overlap(this.player, type, () => {
+                for (let extras of this.artifactGroup.getChildren()) {
+                    extras.setInRange(false);
+                }
                 type.setInRange(true);
             }, null, this);
         }
     }
 
     interactArtifacts() {
-        for (let type of this.artifactGroup.getChildren()) {
-            if (type.getInRange() == true) {
-                if (Phaser.Input.Keyboard.JustDown(keyQ)) {
+        if (Phaser.Input.Keyboard.JustDown(keyQ)) {
+            for (let type of this.artifactGroup.getChildren()) {
+                if (type.getInRange() == true) {
                     if (!this.player.getHolding()) {
                         this.player.setHolding(true);
                         console.log("Q");
                         type.setPickedUp(true);
+                        this.resetSpeed(type);
                     } else {
                         this.player.setHolding(false);
                         console.log("Q");
                         type.setPickedUp(false);
+                        this.resetSpeed(type);
                     }
                 }
             }
         }
+    }
+
+    resetSpeed(artifact) {
+        this.player.setVelocity(0);
+        artifact.setVelocity(0);
     }
 }
